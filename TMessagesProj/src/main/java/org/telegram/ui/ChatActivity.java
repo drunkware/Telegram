0ded4e881f9594b3b8f0f41da8025c76d65e69ea
fduraibi <fadnix@gmail.com>
2014-04-22 16:59:09 -0400

    Maintain the spaces and newline in the message as is

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -857,9 +857,7 @@ public class ChatActivity extends BaseFragment implements SizeNotifierRelativeLa
                         messsageEditText.setText(NewText, TextView.BufferType.EDITABLE);
                     }
 
-                    String message = charSequence.toString().trim();
-                    message = message.replaceAll("\n\n+", "\n\n");
-                    message = message.replaceAll(" +", " ");
+                    String message = charSequence.toString();
                     sendButton.setEnabled(message.length() != 0);
                     checkSendButton();
 
